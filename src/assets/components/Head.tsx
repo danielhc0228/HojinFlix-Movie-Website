0e1d8ba6715bb538955c67051a38b524e7820c14
Daniel Chung <daniel20020228@gmail.com>
2025-02-04 15:01:07 +1000

    Initial commit for Netflix clone. Add background colour and a Box in the center

---
+++ src/assets/components/Head.tsx
@@ -0,0 +1,12 @@
+import { Helmet } from "react-helmet-async";
+
+const Head = () => (
+    <Helmet>
+        <link
+            href='https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap'
+            rel='stylesheet'
+        />
+    </Helmet>
+);
+
+export default Head;
