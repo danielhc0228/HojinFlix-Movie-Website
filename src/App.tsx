ee62c6f926d4d5df2bbfde6ca387a51f34b98ed2
Daniel Chung <daniel20020228@gmail.com>
2025-02-04 15:47:12 +1000

    Add variants and circles inside the box

--- src/App.tsx
+++ src/App.tsx
@@ -12,19 +12,54 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
     width: 200px;
     height: 200px;
-    background-color: white;
-    border-radius: 10px;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    background-color: rgba(255, 255, 255, 0.2);
+    border-radius: 40px;
     box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
+
+const Circle = styled(motion.div)`
+    background-color: white;
+    height: 70px;
+    width: 70px;
+    place-self: center;
+    border-radius: 35px;
+    box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const boxVars = {
+    start: { scale: 0.5, opacity: 0 },
+    end: {
+        scale: 1,
+        opacity: 1,
+        transition: {
+            type: "spring",
+            duration: 0.5,
+            bounce: 0.5,
+            delayChildren: 0.5,
+            staggerChildren: 0.2,
+        },
+    },
+};
+
+const circleVars = {
+    start: { opacity: 0, y: 10 },
+    end: {
+        opacity: 1,
+        y: 0,
+    },
+};
 
 function App() {
     return (
         <Wrapper>
-            <Box
-                transition={{ type: "spring", delay: 0.3 }}
-                initial={{ scale: 0 }}
-                animate={{ scale: 1, rotateZ: 360 }}
-            />
+            <Box variants={boxVars} initial='start' animate='end'>
+                <Circle variants={circleVars} />
+                <Circle variants={circleVars} />
+                <Circle variants={circleVars} />
+                <Circle variants={circleVars} />
+            </Box>
         </Wrapper>
     );
 }
